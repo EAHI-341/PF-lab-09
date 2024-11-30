ac60af484706d81298a83f9e30056c9ef25cf414
EAHI-341 <as27082005@gmail.com>
2024-11-30 14:15:13 +0500

    Update Task 3.cpp

--- Task 3.cpp
+++ Task 3.cpp
@@ -1,1 +1,22 @@
-. 
+#include<iostream>
+using namespace std;
+int main (){
+	
+		int arr[3][3];
+	for(int i=0; i<3; i++){
+		for (int j=0; j<3; j++){
+			
+	cout<<"Enter your numbers:";
+	cin>>arr[i][j];							
+	}					
+		}
+		int sum=0;
+		for(int i=0; i<3; i++){
+		for(int j=0; j<4; j++){
+			cout<<arr[i][j]<<"                ";
+			sum=sum+arr[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"The sum is:"<<sum;
+}
