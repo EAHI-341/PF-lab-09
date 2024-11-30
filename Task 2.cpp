d88537b88596ac473a66332f8a1fb1e70e2373ec
EAHI-341 <as27082005@gmail.com>
2024-11-30 14:14:47 +0500

    Update Task 2.cpp

--- Task 2.cpp
+++ Task 2.cpp
@@ -1,1 +1,21 @@
-. 
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int arr[3][4];
+	for(int i=0; i<3; i++){
+		for (int j=0; j<4; j++){
+			
+	cout<<"Enter your numbers:";
+	cin>>arr[i][j];				
+			
+	}		
+			
+		}
+		for(int i=0; i<3; i++){
+		for(int j=0; j<4; j++){
+			cout<<arr[i][j]<<"                ";
+		}
+		cout<<endl;
+	}
+}
