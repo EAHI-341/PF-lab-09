0bb1d28ab8069261109a4d6d8c3df0578d6c10d9
EAHI-341 <as27082005@gmail.com>
2024-11-30 14:14:01 +0500

    Update Task 1.cpp

--- Task 1.cpp
+++ Task 1.cpp
@@ -1,1 +1,18 @@
-.
+
+#include<iostream>
+using namespace std;
+int main (){
+	
+	int arr[4][4]={
+		{1, 2, 3, 4}, 
+		{6, 7, 8, 9}, 
+		{0, 1,2,3}, 
+		{1, 2, 3, 4}
+	};
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			cout<<arr[i][j]<<"                ";
+		}
+		cout<<endl;
+	}
+}
